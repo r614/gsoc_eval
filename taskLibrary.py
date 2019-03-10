4a3e22499c2c2c1b6837c035193f1d3a86ee81b6
r614 <steradian614@gmail.com>
2019-03-09 22:36:24 -0800

    Added docstrings

--- taskLibrary.py
+++ taskLibrary.py
@@ -12,20 +12,35 @@ import matplotlib.pyplot as plt
 lst = []
 
 def getUTC(filePath, length):
-    """Convert filename from UNIX time in nanoseconds to UTC time."""
+    """Convert filename from UNIX time in nanoseconds to UTC time.
+
+    Takes a file path as input and the length of the UNIXTime chars
+    in the name (18 in the task). Returns a utc datetime
+    object. Assuming input is always in nanoseconds.
+    """
     fileName = os.path.splitext(os.path.basename(filePath))[0]
     unixTime = int(fileName[:length])//(10**9)
     utcTime = utc.localize(datetime.utcfromtimestamp(unixTime))
     return utcTime
 
 def getLocalTime(utcTime, tzone):
-    """Convert utcTime to specific timezone and returns datetime obj."""
+    """Convert utcTime to specific timezone and returns datetime obj.
+
+    Simple function that takes a utcTime obj and pytz timezone as input
+    and returns a localized datetime obj.
+    """
     localTime = utcTime.astimezone(tzone)
     return localTime
 
 
 def getCSV(filePath, name):
-    """Open .h5 file and classifies contents as specified in the GSOC Task."""
+    """Open .h5 file and classifies contents as specified in the GSOC Task.
+
+    Uses the classify helper function to recursively reach every node and
+    classifies using h5py types. For now, numpy TypeErrors are handled via
+    'UnknownType'. Furthermore, some attributes do not apply to Groups and
+    are represented by 'None'
+    """
     categories = ['Name','Structure', 'DataType', 'Shape', 'Size']
     fileInput = h5py.File(filePath, 'r')
     fileInput.visititems(classify)
@@ -34,14 +49,18 @@ def getCSV(filePath, name):
     return db
 
 def arrayPNG(imageData, imageHeight, imageWidth):
-    """Convert 1D array of image data to 2D png output image."""
+    """Convert 1D array of image data to 2D png output image.
+
+    Takes arrays ImageData, ImageHeight and ImageWidth as input.
+    Exports a PNG image.
+    """
     imageArray = np.reshape(imageData, (imageHeight[0], imageWidth[0]))
     img = medfilt(imageArray)
     imgplot = plt.imshow(img)
     plt.imsave('test.png', img)
 
 def classify(name, obj):
-    """Func to recursively iterate .h5 file and return rows in a df."""
+    """Callable Func to recursively iterate .h5 file and return rows in a df."""
     name = name.split("/")[-1]
     if(isinstance(obj, h5py.Dataset)):
         try:
